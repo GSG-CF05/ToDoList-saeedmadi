0bfef6a76aac4f74c8243df3a14863a01a109abf
Saeed99Madi <saed.zooro@gmail.com>
2022-03-04 23:42:04 +0200

    js-f- add elements by js

--- todo.js
+++ todo.js
@@ -5,15 +5,20 @@
 window.addEventListener('load', () => {
   const form = document.querySelector('#new-to-do-form');
   const input = document.querySelector('#new-to-do-input');
-  const submit = document.querySelector('#new-to-do-submit');
+  const list_el = document.querySelector('#to-dos');
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     const task = input.value;
     if (!task) {
       alert('please fill out the task');
       return;
-    } else {
-      console.log('Success');
     }
+    const todo_el = document.createElement('div');
+    todo_el.classList.add('to-do-c');
+    const todo_content_el = document.createElement('div');
+    todo_content_el.classList.add('content');
+    todo_content_el.innerText = task;
+    todo_el.appendChild(todo_content_el);
+    list_el.appendChild(todo_el);
   });
 });
